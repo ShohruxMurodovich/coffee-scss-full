0aa52cb8d8e36889938af2e5ecfda8bebf4bd663
ShohruxMurodovich <murodovichshohruh@gmail.com>
2022-01-19 20:07:09 +0500

    add js file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-let elHeader = document.querySelector("site-header")
-let elHeaderHamburgBtn = document.querySelector("site-header__hamburg-btn")
-
+let elHeader = document.querySelector(".site-header");
+let elHeaderHamburgBtn = document.querySelector(".site-header__btn");
 
 elHeaderHamburgBtn.addEventListener('click', function(){
-  elHeader.classList.toggle("site-header--open");
-});
+  elHeader.classList.toggle("site-header--active");
+})
